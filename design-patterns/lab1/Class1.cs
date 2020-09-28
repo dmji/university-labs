6021120f4301330f29d5c17b0ba22abdbfc1dd60
daimonji <leyshkinanton@gmail.com>
2020-09-28 14:14:16 +0300

    lab1

---
+++ design-patterns/lab1/Class1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace lab1
+{
+    public interface IPrinter
+    {
+        void Print(string obj);
+        void Print(char obj);
+    }
+
+    public interface IPrinterDelegate : IPrinter
+    {
+        public void Print(IPrintable obj);
+    }
+
+    public interface IPrintable
+    {
+        void Print(IPrinter obj);
+    }
+
+    public class PrinterDefault : IPrinter
+    {
+        public virtual void Print(string obj)
+        {
+            Console.Write(obj);
+        }
+        public virtual void Print(char obj)
+        {
+            Console.Write(obj);
+        }
+    }
+
+    public class PrinterDelegate : IPrinterDelegate
+    {
+        IPrinter prn;
+        public PrinterDelegate(IPrinter obj)
+        {
+            prn = obj;
+        }
+        public virtual void Print(string obj)
+        {
+            prn.Print(obj);
+        }
+        public virtual void Print(char obj)
+        {
+            prn.Print(obj);
+        }
+        public void Print(IPrintable obj)
+        {
+            obj.Print(this);
+        }
+    }
+
+
+    public class PrinterSpecial : PrinterDefault
+    {
+        public override void Print(string obj)
+        {
+            base.Print("(" + obj + ")");
+        }
+    }
+
+
+    public class Word : IPrintable
+    {
+        private string word;
+        public Word(string init)
+        {
+            word = init;
+        }
+
+        public void Print(IPrinter obj)
+        {
+            obj.Print(word);
+        }
+    }
+
+    public class Sign : IPrintable
+    {
+        private char word;
+        public Sign(char init)
+        {
+            word = init;
+        }
+
+        public void Print(IPrinter obj)
+        {
+            obj.Print(word);
+        }
+    }
+
+    public class Text : IPrintable
+    {
+        private IPrintable [] word;
+        public Text(IPrintable [] init)
+        {
+            word = init;
+        }
+
+        public void Print(IPrinter obj)
+        {
+            foreach(IPrintable a in word)
+                a.Print(obj);
+        }
+    }
+}
---
+++ design-patterns/lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPrintable[] text = {new Word("Тестируем"), new Sign(' '),
+                new Word("мою"), new Sign(' '),
+                new Word("архитектуру"), new Sign('!') };
+            /*Text txt = new Text(
+                new Word("Тестируем"), new Sign(' '),
+                new Word("мою"), new Sign(' '),
+                new Word("архитектуру"), new Sign('!') );
+            */
+            Text txt = new Text(text);
+
+            IPrinterDelegate prn = new PrinterDelegate(new PrinterSpecial());
+            
+            
+            txt.Print(prn);
+            Console.Write('\n');
+            prn.Print(txt);
+        }
+    }
+}
